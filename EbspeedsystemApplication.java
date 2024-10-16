582e47fe92b4ca674b3a1b67f8bbf2a03b373374
vijayanMC <155290584+vijayanMC@users.noreply.github.com>
2024-10-16 23:23:19 +0530

    Add files via upload

---
+++ EbspeedsystemApplication.java
@@ -0,0 +1,13 @@
+package ebspeedsystem.ebspeedsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbspeedsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbspeedsystemApplication.class, args);
+	}
+
+}
